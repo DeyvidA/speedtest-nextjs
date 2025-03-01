d97c22abe12f10bad6aba0972f874f8edd842e53
DeyvidA <darauz@nicarao.agency>
2025-03-01 10:37:49 -0600

    Create API CRUD BLOGS

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,9 +1,68 @@
-import { NextResponse } from "next/server";
+import { createClient } from "@/helpers/supabase/server";
+
+function slugify(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+  str = str.toLowerCase(); // convert string to lowercase
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+           .replace(/\s+/g, '-') // replace spaces with hyphens
+           .replace(/-+/g, '-'); // remove consecutive hyphens
+  return str;
+}
+
 
 export async function GET() {
-  const users = [
-    { id: 1, name: "John Doe", email: "john@example.com" },
-    { id: 2, name: "Jane Doe", email: "jane@example.com" },
-  ];
-  return Response.json(users);
+  const supabase = await createClient()
+  
+  const {data, error}  = await supabase.from('blogs').select();
+
+
+  return Response.json({ success: true, data })
 }
+
+export async function POST(req) {
+  const supabase = await createClient()
+  
+  const res = await req.json();
+  const {title, content} = res
+
+  const slug = slugify(title);
+
+  const { error } = await supabase
+  .from('blogs')
+  .insert({title, content, slug})
+
+
+  return Response.json({ success: true, data })
+}
+
+export async function PATCH(req) {
+  const supabase = await createClient()
+  
+  const res = await req.json();
+  const {id, title, content} = res
+
+  const slug = slugify(title);
+
+  const { data, error } = await supabase
+  .from('blogs')
+  .update({title, content, slug})
+  .eq('id', id)
+
+  return Response.json({ success: true, data })
+}
+
+export async function DELETE(req) {
+  const supabase = await createClient()
+
+  const res = await req.json();
+  const {id} = res
+
+  const {data} = await supabase
+  .from('blogs')
+  .delete()
+  .eq('id', id)
+
+
+  return Response.json({ success: true, data })
+}
+
